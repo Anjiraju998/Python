eca4fa2dab3b6e70ac0da235f0e727ab1fe4443f
Anjiraju998 <107533726+Anjiraju998@users.noreply.github.com>
2023-01-20 13:13:27 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,17 @@
+html = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sample HTML Page</title>
+</head>
+<body>
+<h1>Hello World!</h1>
+<p>This is a sample HTML page generated using Python.</p>
+</body>
+</html>
+"""
 
+ 
+
+with open("sample.html", "w") as f:
+    f.write(html)
